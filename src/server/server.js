1b73ac4d2e37977fc9da79586defc1eb24528ee7
leuchtend5 <zxcmj1@gmail.com>
2024-05-23 22:42:32 +0800

    feat: add express static to serve images

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,25 @@
 const express = require("express");
-const fs = require("fs");
+const cors = require("cors");
+const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+// serving images file
+app.use(express.static("./src"));
+
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: "GET,PUT,POST,DELETE",
+    optionsSuccessStatus: 204,
+  })
+);
+
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
